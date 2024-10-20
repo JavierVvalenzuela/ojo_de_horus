91f89f98d8b9cccffe06cb8770ff788a38241d82
javier <jav.valenzuelae@duocuc.cl>
2024-10-20 20:08:20 -0300

    optimisacion de la base de datos

--- src/app/services/servicio-bd.service.ts
+++ src/app/services/servicio-bd.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 import { AlertController, Platform } from '@ionic/angular';
-import { BehaviorSubject, filter, Observable, take } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
 import { Usuario } from '../model/usuario';
 
 @Injectable({
@@ -14,20 +15,20 @@ export class ServicioBDService {
   tablaRol: string = "CREATE TABLE IF NOT EXISTS ROL (id_rol INTEGER PRIMARY KEY AUTOINCREMENT, nombre_rol VARCHAR(50) NOT NULL);";
   tablaCategoria: string = "CREATE TABLE IF NOT EXISTS CATEGORIA (id_categoria INTEGER PRIMARY KEY AUTOINCREMENT, nombre_cat VARCHAR(50) NOT NULL);";
   tablaEstado: string = "CREATE TABLE IF NOT EXISTS ESTADO (id_estado INTEGER PRIMARY KEY AUTOINCREMENT, nombre_e VARCHAR(50) NOT NULL);";
-  tablaUsuario: string = `CREATE TABLE IF NOT EXISTS USUARIO (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT, nombre_u VARCHAR(50) NOT NULL, apellido_u VARCHAR(50) NOT NULL, nick_u VARCHAR(50) NOT NULL, correo_u VARCHAR(100) NOT NULL, contrasena_u VARCHAR(50) NOT NULL, pregunta_respaldo_u VARCHAR(255), imagen_perfil_u BLOB, estado_cuenta_u CHAR(1) NOT NULL, razon_ban_u TEXT, id_rol INTEGER NOT NULL, FOREIGN KEY(id_rol) REFERENCES ROL(id_rol));`;
+  tablaUsuario: string = `CREATE TABLE IF NOT EXISTS USUARIO (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT, nombre_u VARCHAR(50) NOT NULL, apellido_u VARCHAR(50) NOT NULL, nick_u VARCHAR(50) NOT NULL UNIQUE, correo_u VARCHAR(100) NOT NULL, contrasena_u VARCHAR(50) NOT NULL, pregunta_respaldo_u VARCHAR(255), imagen_perfil_u BLOB, estado_cuenta_u CHAR(1) NOT NULL, razon_ban_u TEXT, id_rol INTEGER NOT NULL, FOREIGN KEY(id_rol) REFERENCES ROL(id_rol));`;
   tablaPost: string = `CREATE TABLE IF NOT EXISTS POST (id_post INTEGER PRIMARY KEY AUTOINCREMENT, titulo_post VARCHAR(50) NOT NULL, contenido_post TEXT NOT NULL, f_creacion_post DATE NOT NULL, imagen_post BLOB, id_usuario INTEGER NOT NULL, id_estado INTEGER NOT NULL, FOREIGN KEY(id_usuario) REFERENCES USUARIO(id_usuario), FOREIGN KEY(id_estado) REFERENCES ESTADO(id_estado));`;
   tablaComentario: string = `CREATE TABLE IF NOT EXISTS COMENTARIO (id_coment INTEGER PRIMARY KEY AUTOINCREMENT, contenido_coment TEXT NOT NULL, f_creacion_coment DATE NOT NULL, imagen_coment BLOB, id_estado INTEGER NOT NULL, id_post INTEGER NOT NULL, FOREIGN KEY(id_estado) REFERENCES ESTADO(id_estado), FOREIGN KEY(id_post) REFERENCES POST(id_post));`;
   tablaPostCategoria: string = `CREATE TABLE IF NOT EXISTS POST_CATEGORIA (id_post_c INTEGER PRIMARY KEY AUTOINCREMENT, id_categoria INTEGER NOT NULL, id_post INTEGER NOT NULL, FOREIGN KEY(id_categoria) REFERENCES CATEGORIA(id_categoria), FOREIGN KEY(id_post) REFERENCES POST(id_post));`;
   tablaFavoritos: string = `CREATE TABLE IF NOT EXISTS FAVORITOS (id_fav INTEGER PRIMARY KEY AUTOINCREMENT, f_creacion_fav DATE NOT NULL, id_usuario INTEGER NOT NULL, id_post INTEGER NOT NULL, FOREIGN KEY(id_usuario) REFERENCES USUARIO(id_usuario), FOREIGN KEY(id_post) REFERENCES POST(id_post));`;
 
-  listadoUsuarios = new BehaviorSubject<Usuario[]>([]);
-  listadoPost = new BehaviorSubject([]);
-  listadoComentario = new BehaviorSubject([]);
-  listadoFavoritos = new BehaviorSubject([]);
-
+  private listadoUsuarios = new BehaviorSubject<Usuario[]>([]);
   private isBDReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor(private SQLite: SQLite, private platform: Platform, private alertController: AlertController) {
+  constructor(
+    private SQLite: SQLite,
+    private platform: Platform,
+    private alertController: AlertController
+  ) {
     this.createBD();
   }
 
@@ -40,12 +41,12 @@ export class ServicioBDService {
     await alert.present();
   }
 
-  fetchUsuario(): Observable<Usuario[]> {
+  fetchUsuarios(): Observable<Usuario[]> {
     return this.listadoUsuarios.asObservable();
   }
 
-  dbState() {
-    return this.isBDReady.asObservable(); 
+  dbState(): Observable<boolean> {
+    return this.isBDReady.asObservable();
   }
 
   createBD() {
@@ -55,9 +56,7 @@ export class ServicioBDService {
         location: 'default'
       }).then((db: SQLiteObject) => {
         this.database = db;
-        this.crearTablas(); 
-        this.isBDReady.next(true);
-        this.createInitialUser(); // Crear usuario inicial
+        this.crearTablas();
       }).catch((e: any) => {
         this.presentAlert('Error de Base de Datos', `No se pudo crear la base de datos: ${e.message}`);
       });
@@ -75,6 +74,7 @@ export class ServicioBDService {
       await this.database.executeSql(this.tablaPostCategoria, []);
       await this.database.executeSql(this.tablaFavoritos, []);
       this.isBDReady.next(true);
+      await this.createInitialUser();
     } catch (e: any) {
       this.presentAlert('Error de Tablas', `No se pudieron crear las tablas: ${e.message}`);
     }
@@ -82,15 +82,13 @@ export class ServicioBDService {
 
   async getAllUsuarios(): Promise<Usuario[]> {
     const query = 'SELECT * FROM USUARIO';
-    const usuarios: Usuario[] = [];
-  
     try {
-      await this.isBDReady.pipe(filter((ready: any) => ready), take(1)).toPromise();
-  
       const result = await this.database.executeSql(query, []);
+      const usuarios: Usuario[] = [];
       for (let i = 0; i < result.rows.length; i++) {
         usuarios.push(result.rows.item(i));
       }
+      this.listadoUsuarios.next(usuarios);
       return usuarios;
     } catch (e: any) {
       this.presentAlert('Error', `No se pudieron obtener los usuarios: ${e.message}`);
@@ -102,10 +100,7 @@ export class ServicioBDService {
     const query = 'SELECT * FROM USUARIO WHERE nick_u = ?';
     try {
       const result = await this.database.executeSql(query, [nick]);
-      if (result.rows.length > 0) {
-        return result.rows.item(0);
-      }
-      return null;
+      return result.rows.length > 0 ? result.rows.item(0) : null;
     } catch (e: any) {
       this.presentAlert('Error', `No se pudo obtener el usuario: ${e.message}`);
       return null;
@@ -122,26 +117,25 @@ export class ServicioBDService {
       this.presentAlert('Error', 'El nombre de usuario ya existe. Por favor, elige otro.');
       return;
     }
-  
+
     try {
       await this.isBDReady.pipe(filter(ready => ready), take(1)).toPromise();
-  
+
       const query = `INSERT INTO USUARIO (nombre_u, apellido_u, nick_u, correo_u, contrasena_u, estado_cuenta_u, id_rol) 
                    VALUES (?, ?, ?, ?, ?, 'A', ?)`;
-  
+
       await this.database.executeSql(query, [nombre, apellido, nick, correo, contrasena, idRol]);
       this.presentAlert('Éxito', 'Usuario insertado correctamente.');
-      this.cargarUsuarios();
+      await this.cargarUsuarios();
     } catch (e: any) { 
       this.presentAlert('Error', `Error al insertar usuario: ${e.message}`);
     }
   }
 
-  // usuario Diego
   private async createInitialUser() {
     const nick = 'Diego';
     if (!(await this.usuarioExists(nick))) {
-      await this.insertarUsuario('Diego', 'Mellado', nick, 'diego@example.com', 'Diego170', 1); // Cambiar el idRol
+      await this.insertarUsuario('Diego', 'Mellado', nick, 'diego@example.com', 'Diego170', 1);
     }
   }
 
@@ -151,7 +145,7 @@ export class ServicioBDService {
     try {
       await this.database.executeSql(query, [usuario.nombre_u, usuario.apellido_u, usuario.correo_u, usuario.contrasena_u, usuario.estado_cuenta_u, usuario.razon_ban_u, usuario.id_usuario]);
       this.presentAlert('Éxito', 'Datos del usuario actualizados correctamente.');
-      this.cargarUsuarios();
+      await this.cargarUsuarios();
     } catch (e: any) { 
       this.presentAlert('Error', `Error al actualizar usuario: ${e.message}`);
     }
@@ -163,6 +157,7 @@ export class ServicioBDService {
     try {
       await this.database.executeSql(query, [idRol, nick]);
       this.presentAlert('Éxito', 'Rol asignado correctamente.');
+      await this.cargarUsuarios();
     } catch (e: any) { 
       this.presentAlert('Error', `Error al asignar rol: ${e.message}`);
     }
